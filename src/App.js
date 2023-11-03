cb30155258ecdc996c696149ca4ee214a9495546
Mikheil Gogia <m.gogia@hotmail.com>
2023-11-04 01:04:35 +0400

    docs: added functionality to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { ChatEngine} from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return <div></div>;
+  if(!localStorage.getItem('username')) return <LoginForm />
+
+  return (
+    <div>
+      <ChatEngine
+        height="100vh"
+        projectID="27894a81-babb-4966-8ca1-dd83cacf0f2d"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    </div>
+  );
 }
 
 export default App;
